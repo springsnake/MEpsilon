6927e8c7ea8e5263b15c176b9c23663455421f95
prophetofyig <prophetofyig@gmail.com>
2020-04-16 02:27:40 +0300

    Add project files.

---
+++ MachinEpsilon/Source.cpp
@@ -0,0 +1,40 @@
+# include <iostream>
+# include <stdint.h>
+# include <iomanip>
+
+template<typename float_t, typename int_t>
+float_t machine_eps()
+{
+	union
+	{
+		float_t f;
+		int_t   i;
+	} one, one_plus, little, last_little;
+
+	one.f = 1.0;
+	little.f = 1.0;
+	last_little.f = little.f;
+
+	while (true)
+	{
+		one_plus.f = one.f;
+		one_plus.f += little.f;
+
+		if (one.i != one_plus.i)
+		{
+			last_little.f = little.f;
+			little.f /= 2.0;
+		}
+		else
+		{
+			return last_little.f;
+		}
+	}
+}
+
+int main()
+{
+	std::cout << "machine epsilon:\n";
+	std::cout << "float: " << std::setprecision(18) << machine_eps<float, uint32_t>() << std::endl;
+	std::cout << "double: " << std::setprecision(18) << machine_eps<double, uint64_t>() << std::endl;
+}
