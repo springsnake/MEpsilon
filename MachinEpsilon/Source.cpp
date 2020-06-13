a99ef4839e4f1d2d4de826cefe6f9d23a47d630b
prophetofyig <prophetofyig@gmail.com>
2020-06-13 17:18:06 +0300

    .

--- MachinEpsilon/Source.cpp
+++ MachinEpsilon/Source.cpp
@@ -1,12 +1,11 @@
-# include <iostream>
-# include <stdint.h>
-# include <iomanip>
+#include <iostream>
+#include <stdint.h>
+#include <iomanip>
 
 template<typename float_t, typename int_t>
 float_t machine_eps()
 {
-	union
-	{
+	union {
 		float_t f;
 		int_t   i;
 	} one, one_plus, little, last_little;
@@ -15,18 +14,14 @@ float_t machine_eps()
 	little.f = 1.0;
 	last_little.f = little.f;
 
-	while (true)
-	{
+	while (true) {
 		one_plus.f = one.f;
 		one_plus.f += little.f;
-
 		if (one.i != one_plus.i)
 		{
 			last_little.f = little.f;
 			little.f /= 2.0;
-		}
-		else
-		{
+		} else {
 			return last_little.f;
 		}
 	}
@@ -34,7 +29,8 @@ float_t machine_eps()
 
 int main()
 {
-	std::cout << "machine epsilon:\n";
-	std::cout << "float: " << std::setprecision(18) << machine_eps<float, uint32_t>() << std::endl;
-	std::cout << "double: " << std::setprecision(18) << machine_eps<double, uint64_t>() << std::endl;
+	using namespace std;
+	cout << "machine epsilon:\n";
+	cout << "float: " << setprecision(18) << machine_eps<float, uint32_t>() << endl;
+	cout << "double: " << setprecision(18) << machine_eps<double, uint64_t>() << endl;
 }
